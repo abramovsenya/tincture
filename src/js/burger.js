765d1ff5642050336c3001ebabd70e30b1d9156f
arseniyabramov <abramovarsenij2005@gmail.com>
2023-10-10 23:10:40 +0300

    fix: burger can close

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,8 +1,14 @@
 const menuBtn = document.querySelector('.header__burger');
-
 const menu = document.querySelector('.header__nav-mobile');
+const menuClose = document.querySelectorAll('.header__menu-mobile-link');
 
 menuBtn.addEventListener('click', () => {
 	menu.classList.toggle('show');
 	menuBtn.classList.toggle('active');
 });
+
+menuClose.forEach(link =>
+	link.addEventListener('click', () => {
+		menuBtn.classList.remove('active'), menu.classList.remove('show');
+	})
+);
