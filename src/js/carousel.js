52d92f6394e6410de3624b33c1396f2faa2d3e1c
arseniyabramov <abramovarsenij2005@gmail.com>
2023-10-11 00:02:13 +0300

    feature: slider test

---
+++ src/js/carousel.js
@@ -0,0 +1,41 @@
+const swiper = new Swiper('.swiper', {
+	spaceBetween: 30,
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	pagination: {
+		el: '.swiper-pagination',
+		dynamicBullets: true,
+		dynamicMainBullets: 3,
+	},
+	mousewheel: {
+		enabled: false,
+	},
+	touch: {
+		enabled: false,
+	},
+
+	autoplay: { delay: '5000' },
+
+	loop: true,
+	lazyPreloadPrevNext: 4,
+	slidesPerView: 1,
+	breakpoints: {
+		768: {
+			slidesPerView: 2,
+			spaceBetween: 20,
+		},
+		1024: {
+			slidesPerView: 3,
+			spaceBetween: 40,
+		},
+		1440: {
+			slidesPerView: 4,
+			spaceBetween: 50,
+		},
+	},
+
+	mousewheel: true,
+	keyboard: true,
+});
