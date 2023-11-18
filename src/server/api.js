e3ec5977fceeefe56340804cce3180f1a0a0bd66
arseniyabramov <abramovarsenij2005@gmail.com>
2023-11-18 15:19:49 +0300

    fix: swiper fixing

--- src/server/api.js
+++ src/server/api.js
@@ -78,57 +78,99 @@ promise
 	.catch(error => {
 		console.log(error);
 	});
+// import { promise } from './fetch.js';
 
-/*
-	
-const containers = document.querySelectorAll('.swiper-wrapper');
+// promise
+// 	.then(products => {
+// 		const categories = {}; // Создаем объект для хранения товаров по категориям
+// 		products.forEach(product => {
+// 			if (!categories[product.category]) {
+// 				categories[product.category] = []; // Если категория еще не создана, создаем новый массив для товаров этой категории
+// 			}
+// 			categories[product.category].push(product); // Добавляем товар в соответствующую категорию
+// 		});
 
-containers.forEach(container => {
-  const products = container.querySelectorAll('.swiper-slide');
+// 		const swiperContainers = document.querySelectorAll('.swiper');
 
-  products.forEach(product => {
-    // обработка каждого элемента внутри контейнера
-    // например, изменение стилей, добавление классов и т. д.
-  });
-});
- 
-	*/
-/* import { promise } from './fetch.js';
+// 		Object.keys(categories).forEach(category => {
+// 			const categoryProducts = categories[category];
+// 			const swiperContainer = document.createElement('div');
+// 			swiperContainer.classList.add('swiper');
+// 			const productsContainer = document.createElement('div');
+// 			productsContainer.classList.add('swiper-wrapper');
 
-promise
-	.then(products => {
-		const firstProductsContainer = document.querySelector(
-			'.swiper-wrapper:first-child'
-		);
-		const secondProductsContainer = document.querySelector(
-			'.swiper-wrapper:last-child'
-		);
+// 			categoryProducts.forEach(post => {
+// 				const productsElement = document.createElement('div');
+// 				productsElement.classList.add('swiper-slide');
+// 				productsElement.innerHTML = `<a class='swiper-slide__link' href='#'>
+// 					<img
+// 						class='swiper-slide__image'
+// 						src='${post.image}'
+// 						alt='eda'
+// 					/>
+// 					<div class='swiper-slide__about-inner'>
+// 						<p class='swiper-slide__about-inner-name'>${post.name}</p>
+// 						<p class='swiper-slide__about-inner-about'>${post.descriptions}</p>
+// 						<div class='swiper-slide__about-inner-down'>
+// 							<p class='swiper-slide__about-inner-down-price'>${post.price} ₽</p>
+// 							<a class='swiper-slide__about-inner-down-btn' href='#'>
+// 								<img src='/src/assets/icons/corzina.svg' alt='corzina' />В заказ
+// 							</a>
+// 						</div>
+// 					</div>
+// 				</a>`;
+// 				console.log(productsElement);
+// 				productsContainer.appendChild(productsElement);
+// 			});
 
-		products.forEach(post => {
-			const productsElement = document.createElement('div');
-			productsElement.classList.add('swiper-slide');
-			productsElement.innerHTML = `<a class='swiper-slide__link' href='#'>
-			<img
-				class='swiper-slide__image'
-				src='${post.image}'
-				alt='eda'
-			/>
-			<div class='swiper-slide__about-inner'>
-				<p class='swiper-slide__about-inner-name'>${post.name}</p>
-				<p class='swiper-slide__about-inner-about'>${post.descriptions}</p>
-				<div class='swiper-slide__about-inner-down'>
-					<p class='swiper-slide__about-inner-down-price'>${post.price} ₽</p>
-					<a class='swiper-slide__about-inner-down-btn' href='#'>
-						<img src='/src/assets/icons/corzina.svg' alt='corzina' />В заказ
-					</a>
-				</div>
-			</div>
-		</a>`;
-			console.log(firstProductsContainer);
-			firstProductsContainer.appendChild(productsElement.cloneNode(true));
-			secondProductsContainer.appendChild(productsElement);
-		});
-	})
-	.catch(error => {
-		console.log(error);
-	});*/
+// 			swiperContainer.appendChild(productsContainer);
+
+// 			document.body.appendChild(swiperContainer);
+
+// 			new Swiper(swiperContainer, {
+// 				// настройки слайдера
+// 				spaceBetween: 30,
+// 				navigation: {
+// 					nextEl: '.swiper-button-next',
+// 					prevEl: '.swiper-button-prev',
+// 				},
+// 				pagination: {
+// 					el: '.swiper-pagination',
+// 					dynamicBullets: true,
+// 					dynamicMainBullets: 1,
+// 				},
+// 				mousewheel: {
+// 					enabled: false,
+// 				},
+// 				touch: {
+// 					enabled: false,
+// 				},
+
+// 				autoplay: { delay: '5000' },
+
+// 				loop: true,
+// 				lazyPreloadPrevNext: 4,
+// 				slidesPerView: 1,
+// 				breakpoints: {
+// 					768: {
+// 						slidesPerView: 2,
+// 						spaceBetween: 20,
+// 					},
+// 					1024: {
+// 						slidesPerView: 3,
+// 						spaceBetween: 40,
+// 					},
+// 					1440: {
+// 						slidesPerView: 4,
+// 						spaceBetween: 50,
+// 					},
+// 				},
+
+// 				mousewheel: true,
+// 				keyboard: true,
+// 			});
+// 		});
+// 	})
+// 	.catch(error => {
+// 		console.log(error);
+// 	});
